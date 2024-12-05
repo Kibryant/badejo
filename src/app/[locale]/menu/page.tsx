06a50431fad29775d1e1a84b4bfde0b9d4387c6d
Kibryant <arthurgustavon@gmail.com>
2024-12-05 19:06:44 -0300

    add suspense

--- src/app/[locale]/menu/page.tsx
+++ src/app/[locale]/menu/page.tsx
@@ -2,6 +2,7 @@ import { initTranslations } from '@/app/i18n'
 import { LanguageChanger } from '@/components/language-changer'
 import { SeafoodMenu } from '@/components/seafood-menu'
 import { TranslationsProvider } from '@/providers/translations-provider'
+import { Suspense } from 'react'
 
 interface PageProps {
   params: {
@@ -15,26 +16,28 @@ export default async function Page({ params: { locale } }: PageProps) {
   const { t, resources } = await initTranslations(locale, namespace)
 
   return (
-    <TranslationsProvider
-      resources={resources}
-      locale={locale}
-      namespaces={namespace}
-    >
-      <div className="min-h-screen bg-[#FCFDFE] text-[#000000]">
-        <div className="bg-[#7E4108] text-center p-4 md:px-44">
-          <LanguageChanger />
-        </div>
-        <main>
-          <section className="container mx-auto py-16 sm:py-40 px-4">
-            <h1 className="text-5xl font-bold mb-3 text-center text-[#7E4108]">
-              {t('Nosso Cardápio')}
-            </h1>
-            <div className="w-24 h-1 bg-[#7E4108] mx-auto mb-12" />
+    <Suspense fallback="loading...">
+      <TranslationsProvider
+        resources={resources}
+        locale={locale}
+        namespaces={namespace}
+      >
+        <div className="min-h-screen bg-[#FCFDFE] text-[#000000]">
+          <div className="bg-[#7E4108] text-center p-4 md:px-44">
+            <LanguageChanger />
+          </div>
+          <main>
+            <section className="container mx-auto py-16 sm:py-40 px-4">
+              <h1 className="text-5xl font-bold mb-3 text-center text-[#7E4108]">
+                {t('Nosso Cardápio')}
+              </h1>
+              <div className="w-24 h-1 bg-[#7E4108] mx-auto mb-12" />
 
-            <SeafoodMenu namespace={namespace} />
-          </section>
-        </main>
-      </div>
-    </TranslationsProvider>
+              <SeafoodMenu namespace={namespace} />
+            </section>
+          </main>
+        </div>
+      </TranslationsProvider>
+    </Suspense>
   )
 }
